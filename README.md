a8afcd4bf938bad34377f35da718ec49495e8253
s223484777 <162289887+s223484777@users.noreply.github.com>
2024-03-06 13:42:10 +1100

    Comments and README
    Added better comments to the code files, and made a proper README file for the repo.

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
 # SIT210-Task1.1PBlinkTim
- S306 SIT210 Task 1.1P, blinking an LED in morse code.
+S306 SIT210 Task 1.1P, blink a student's name in morse code whenever a button is pressed.
+
+## Description
+The pin definitions in the main file (morseLEDv1_0.ino) are for the ESP8266 based D1 Mini board. These definitions may be removed or modified for other boards
+The morse code timing follows <https://www.itu.int/dms_pubrec/itu-r/rec/m/R-REC-M.1677-1-200910-I!!PDF-E.pdf>.
+
+Connect an LED to the `morseLED`, and a button (pulled down) to `buttonPin`. Whenever the button is pressed, the sketch will translate the string to morse code, and blink the morse code using the LED.
+
+## How it Works
+The morse code representation of a character is obtained by first converting the character to uppercase, followed by looking up the morse byte in either the letters or numbers lookup. If a character is not in either of them or a space, the morse byte defaults to a "?", or whichever byte is defined in the definitions section.
+
+
+The following snippet (which does most of the heavy lifting) reads the morse code byte bit by bit, blinking a dit for a 1, and blinking a dah for a 0. Before any symbols are output, the loop must reach the first 1, allowing the length of each character to vary from 0 to 7 symbols.
+```cpp
+bool inMorse = false;
+for(int i = 7; i >= 0; i--){
+  if(inMorse){
+    if((morse >> i) & 0b1){
+      dit();
+    }else{
+      dah();
+    }
+    delay(unit_time * symbol_space);
+  }else{
+    if((morse >> i) & 0b1){
+      inMorse = true;
+    }
+  }
+}
+```
+
+To blink an entire string, the `blinkMorseString()` simply iterates through the string character by character, calling `blinkMorseChar()` each time. Calls to `blinkMorseChar()` have a final delay of one letter space, and calls to `blinkMorseString()` have a final delay of 2 word spaces so there is a clear separation of strings.
+
+## Usage
+All that is required for this sketch is to call `blinkMorseString()` with whatever string you'd like to blink on the `morseLED` pin. As per the task sheet, this is called with my name whenever the button is pressed.
--- morse.ino
+++ morse.ino
@@ -1,12 +1,15 @@
+// Definitions
 #define unit_time           125 // Unit time in ms
+#define unknown      0b01110011 // Question mark for any unknown chars
+
 // Following: https://www.itu.int/dms_pubrec/itu-r/rec/m/R-REC-M.1677-1-200910-I!!PDF-E.pdf
 #define dit_units             1 // Units per dit
 #define dah_units             3 // Units per dah
-#define inner_space           1 // Units between each dit/dah within a letter
+#define symbol_space          1 // Units between each dit/dah within a letter
 #define letter_space          3 // Units between each letter
 #define word_space            7 // Units between each word
-#define unknown      0b01110011 // Question mark for any unknown chars
 
+// Define the morse representation for letters
 byte lookup_morse_letters[] = {
   0b00000110,
   0b00010111,
@@ -36,6 +39,7 @@ byte lookup_morse_letters[] = {
   0b00010011
 };
 
+// Define the morse representation for numbers
 byte lookup_morse_numbers[] = {
   0b00100000,
   0b00110000,
@@ -49,19 +53,23 @@ byte lookup_morse_numbers[] = {
   0b00100001
 };
 
+// Blink a short dit (dot)
 void dit(){
   digitalWrite(morseLED, HIGH);
   delay(unit_time * dit_units);
   digitalWrite(morseLED, LOW);
 }
 
+// Blink a long dah (dash)
 void dah(){
   digitalWrite(morseLED, HIGH);
   delay(unit_time * dah_units);
   digitalWrite(morseLED, LOW);
 }
 
+// Blink the morse code for a given character (letters and numbers only).
 void blinkMorseChar(char c){
+  // Find the morse representaion for the char, or use "?" if unknown
   c = toupper(c);
   byte morse = unknown;
   if(c > 47 && c < 58){        // Numbers
@@ -73,6 +81,7 @@ void blinkMorseChar(char c){
     return;
   }
 
+  // Start blinking after the first high bit
   bool inMorse = false;
   for(int i = 7; i >= 0; i--){
     if(inMorse){
@@ -81,19 +90,24 @@ void blinkMorseChar(char c){
       }else{
         dah();
       }
-      delay(unit_time * inner_space);
+      delay(unit_time * symbol_space);
     }else{
       if((morse >> i) & 0b1){
         inMorse = true;
       }
     }
   }
-  delay(unit_time * (letter_space - inner_space));
+
+  // Delay between letter (offset for the symbol_space)
+  delay(unit_time * (letter_space - symbol_space));
 }
 
+// Blink the morse code for a given string.
 void blinkMorseString(char str[]){
   for(int i = 0; i < strlen(str); i++){
     blinkMorseChar(str[i]);
   }
-  delay(unit_time * (word_space - letter_space) * 2); // Ensure consecutive strings are seperated
+
+  // Ensure consecutive strings are seperated
+  delay(unit_time * (word_space - letter_space) * 2);
 }
